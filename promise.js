ab6d53705fc49b533fa2f1d98a91e606f6e8c84a
Clarenti Celsia <xozhengxiao@gmail.com>
2022-09-19 18:22:22 +0700

    [Add] Count people emotion

--- promise.js
+++ promise.js
@@ -1,7 +1,28 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async(emosi) => {
+  try {
+    const theaterIXX = await promiseTheaterIXX();
+    const theaterVGC = await promiseTheaterVGC();
+
+    let angry = 0;
+    let not_angry = 0;
+
+    for (i in theaterIXX){
+      theaterIXX[i].hasil === "marah"? angry++ : not_angry++;
+    }
+
+    for (i in theaterVGC){
+      theaterVGC[i].hasil === "marah"? angry++ : not_angry++;
+    }
+    
+    return emosi === "marah"? angry: not_angry
+
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
   promiseOutput,
